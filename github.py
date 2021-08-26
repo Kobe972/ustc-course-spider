80178b3d2dea59ca51a5d8d376a1cd510380d483
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-27 00:20:26 +0800

    Update github.py

--- github.py
+++ github.py
@@ -1,17 +1,19 @@
 import requests
 from lxml import etree
-name="复变函数"
+name="数据结构"
 counter=0
+flag=0
 content=''
 for page in range(0,20):
     url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
     text=requests.get(url).text
     html=etree.HTML(text)
     repo=html.xpath('.//a[@class="v-align-middle"]/@href')
-    if page==0:
-        print(text)
     for href in repo:
         text=requests.get('https://github.com'+href).text
+        if flag==0:
+            print(text)
+            flag=1
         if name in text:
             content+=name+'\n'
         print('counter:',counter+1)
