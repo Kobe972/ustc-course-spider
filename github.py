74c98cddfbe9d801ee6510a5909593857f6281d9
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-27 00:44:18 +0800

    Update github.py

--- github.py
+++ github.py
@@ -3,31 +3,8 @@ import time
 from lxml import etree
 name="复变函数"
 content=''
-for page in range(0,9):
-    url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
-    text=requests.get(url).text
-    html=etree.HTML(text)
-    repo=html.xpath('.//a[@class="v-align-middle"]/@href')
-    for href in repo:
-        text=requests.get('https://github.com'+href).text
-        if name in text:
-            content+=href+'\n'
-print(content)
-time.sleep(300)
-content=''
-for page in range(9,18):
-    url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
-    text=requests.get(url).text
-    html=etree.HTML(text)
-    repo=html.xpath('.//a[@class="v-align-middle"]/@href')
-    for href in repo:
-        text=requests.get('https://github.com'+href).text
-        if name in text:
-            content+=href+'\n'
-print(content)
-time.sleep(300)
-content=''
-for page in range(18,20):
+for page in range(0,20):
+    time.sleep(5)
     url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
     text=requests.get(url).text
     html=etree.HTML(text)
