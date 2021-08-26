64aca78eff389b4143c7cd8354dbe52f5ea45f41
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-27 00:06:57 +0800

    Update github.py

--- github.py
+++ github.py
@@ -7,11 +7,10 @@ for page in range(0,20):
     url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
     text=requests.get(url).text
     html=etree.HTML(text)
-    repo=html.xpath('//a[@class="v-align-middle"]/@href')
+    repo=html.xpath('.//a[@class="v-align-middle"]/@href')
     print(len(repo))
     for href in repo:
-        text=requests.get('https://github.com/'+href).text
-        print('https://github.com/'+href)
+        text=requests.get('https://github.com'+href).text
         if name in text:
             content+=name+'\n'
         print('counter:',counter+1)
