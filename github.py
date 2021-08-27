3e404780cc01d6b6c1d556547c9f5c59b87ca9f3
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-27 21:07:02 +0800

    Update github.py

--- github.py
+++ github.py
@@ -3,7 +3,7 @@ import requests
 import time
 import json
 from lxml import etree
-from mail import Email
+from mail import Email #如果不用科大邮箱，mail.py取最新一封邮件的代码可能也要改。科大邮箱默认最新未读邮件索引是1
 
 parser = argparse.ArgumentParser(description='Ustc-course Spyder')
 parser.add_argument('data_path', help='path to your own data used for post method', type=str)
@@ -11,10 +11,12 @@ parser.add_argument('login', help='email address or github name', type=str)
 parser.add_argument('password', help='your password of GitHub', type=str)
 parser.add_argument('mailpassword', help='your password of your USTC email', type=str)
 args = parser.parse_args()
-name="复变函数"
+name="数据结构"
 content=''
 progress=0
 session=requests.Session()
+
+#登录github
 form=session.get('https://github.com/login').text
 form=etree.HTML(form)
 authenticity_token=form.xpath('.//input[@name="authenticity_token"]/@value')
@@ -31,18 +33,22 @@ with open(args.data_path, "r+") as f:
     data['password']=args.password
     data[field[0]]=''
 text=session.post('https://github.com/session',data=data).text
+
+#输入邮箱验证码
 if 'Device verification code' in text:
     text=etree.HTML(text)
     authenticity_token=text.xpath('.//input[@name="authenticity_token"]/@value')
     email = args.login
     password = args.mailpassword
-    pop3_server = "mail.ustc.edu.cn"
+    pop3_server = "mail.ustc.edu.cn" #如果不用科大邮箱，这里要改
     LT=None
     while LT==None:
         LT=Email(email,password,pop3_server).get_LT()
     data={'authenticity_token':authenticity_token[0],
           'otp':LT}
     session.post('https://github.com/sessions/verified-device',data=data)
+    
+#搜索仓库
 for page in range(0,20):
     url='https://github.com/search?p='+str(page+1)+'&q=ustc+course&type=Repositories'
     text=session.get(url).text
@@ -52,13 +58,13 @@ for page in range(0,20):
         text=session.get('https://github.com'+href).text
         html=etree.HTML(text)
         branch=html.xpath('.//span[@class="css-truncate-target"]/text()')
-        if(len(branch)==0):
+        if(len(branch)==0): #空仓库没有branch
             print('An error occurred in',href,',maybe it is an empty repository.')
             progress+=1
             continue
         text=session.get('https://github.com'+href+'/find/'+branch[0]).text
         html=etree.HTML(text)
-        data_url=html.xpath('.//fuzzy-list[@class="js-tree-finder"]/@data-url')
+        data_url=html.xpath('.//fuzzy-list[@class="js-tree-finder"]/@data-url') #获取完整文件列表
         if(len(data_url)==0):
             print('Error!')
             print('The spyder has explored',progress,'repositories.')
