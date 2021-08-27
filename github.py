c06c2a5b6ee2dc8ed8e0da8fc9d52f3aae5e6274
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-27 20:24:54 +0800

    Update github.py

--- github.py
+++ github.py
@@ -56,7 +56,6 @@ for page in range(0,20):
         html=etree.HTML(text)
         data_url=html.xpath('.//fuzzy-list[@class="js-tree-finder"]/@data-url')
         if(len(data_url)==0):
-            print(text)
             print(content)
         text=session.get('https://github.com'+data_url[0]).text
         if name in text:
