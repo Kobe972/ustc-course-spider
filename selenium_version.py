b0ac352cf781d3447bd221519b1fdbd921e5e74a
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2022-01-05 22:36:07 +0800

    Update selenium_version.py

--- selenium_version.py
+++ selenium_version.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 from tqdm import tqdm
 from lxml import etree
