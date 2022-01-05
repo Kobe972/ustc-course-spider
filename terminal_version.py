b6a86eb0c3b5c0cc2022810a72851a2958543077
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2022-01-05 22:40:46 +0800

    Update terminal_version.py

--- terminal_version.py
+++ terminal_version.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 import requests
 from tqdm import tqdm
