aa37d13dfde3d8b162ef96d8cc416fb3fa0b7c26
徐亦昶 <71507738+Kobe972@users.noreply.github.com>
2022-06-04 16:30:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import argparse
-keys=['运筹','operation'] #这里放文件名中要有的关键字
+keys=['数理逻辑'] #这里放文件名中要有的关键字
 search='ustc course' #这里放搜索关键字
 parser = argparse.ArgumentParser(description='Ustc-course Spyder')
 parser.add_argument('token', help='token for your github account', type=str)
